53dc1733528223af8a71191cd5407fdfdd6bb73e
mikelpint <mikelaitornube2010@gmail.com>
2017-11-16 22:45:17 +0100

    Updated style and added a Gulpfile

---
+++ Gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var gulp = require('gulp-htmlmin');
+var sass = require('gulp-sass');
+var gulp = require('gulp-inject');
+// Sass
+gulp.task('styles', function() {
+    gulp.src('./sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
+//Watch
+gulp.task('default',function() {
+    gulp.watch('sass/**/*.scss',['styles']);
+});
